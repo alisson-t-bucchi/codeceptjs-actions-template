610f3ab406f2cc07ead8e7cb90ada763d19172ba
alisson-t-bucchi <alisson.bucchi@outlook.com>
2024-09-08 20:27:56 +0100

    simulando bug

--- e2e/login_test.js
+++ e2e/login_test.js
@@ -1,26 +1,26 @@
-Feature('login');
+Feature("login");
 
-Scenario('usuário obrigatório', ({ I }) => {
-    I.login('', 'senha123')
-    I.toast('Informe o seu nome de usuário!')
+Scenario("usuário obrigatório", ({ I }) => {
+  I.login("", "senha123");
+  I.toast("Informe o seu nome de usuário!");
 });
 
-Scenario('senha obrigatória', ({ I }) => {
-    I.login('qa', '')
-    I.toast('Informe a sua senha secreta!')
-})
+Scenario("senha obrigatória", ({ I }) => {
+  I.login("qa", "");
+  I.toast("Informe a sua senha secreta!");
+});
 
-Scenario('usuário não existe', ({ I }) => {
-    I.login('teste', 'teste')
-    I.toast('Oops! Credenciais inválidas :(')
-})
+Scenario("usuário não existe", ({ I }) => {
+  I.login("teste", "teste");
+  I.toast("Oops! Credenciais inválidas :(");
+});
 
-Scenario('senha incorreta', ({ I }) => {
-    I.login('qa', 'teste')
-    I.toast('Oops! Credenciais inválidas :(')
-})
+Scenario("senha incorreta", ({ I }) => {
+  I.login("qa", "teste");
+  I.toast("Oops! Credenciais incorretas :(");
+});
 
-Scenario('com sucesso', ({ I }) => {
-    I.login('qa', 'xperience')
-    I.modal('Suas credenciais são válidas :)')
-})
+Scenario("com sucesso", ({ I }) => {
+  I.login("qa", "xperience");
+  I.modal("Suas credenciais são inválidas :)");
+});
